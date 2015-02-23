9324af47a8b60fbee1b5bf12d284de371f73acca
c457 <android.c357@gmail.com>
2015-02-22 19:18:32 -0600

    gardalte: Cleanup for smdk3470-common
    Change-Id: I6a5d7f793d0668b48218a75f8bcce548f1e1117f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,85 +14,35 @@
 # limitations under the License.
 #
 
-BOARD_VENDOR := samsung
-
-# Architecture
-TARGET_ARCH := arm
-TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
-TARGET_CPU_SMP := true
-TARGET_CPU_VARIANT := cortex-a7
+# inherit from common smdk3470
+-include device/samsung/smdk3470-common/BoardConfigCommon.mk
 
 # Assert
 TARGET_OTA_ASSERT_DEVICE := gardalte,gardalteMetroPCS,gardaltetmo
 
-# Bootloader
-TARGET_BOOTLOADER_BOARD_NAME := universal3470
-TARGET_NO_BOOTLOADER := true
-
 # Filesystems
 BOARD_BOOTIMAGE_PARTITION_SIZE := 8388608
+BOARD_CACHEIMAGE_PARTITION_SIZE := 524288000
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 8388608
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1468006400
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 5679071232
-BOARD_FLASH_BLOCK_SIZE := 4096
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 5679087616
 
 # Include path
 TARGET_SPECIFIC_HEADER_PATH := device/samsung/gardalte/include
 
 # Kernel
-BOARD_KERNEL_BASE := 0x10000000
-BOARD_KERNEL_PAGESIZE := 2048
 TARGET_KERNEL_CONFIG := cyanogenmod_gardalte_defconfig
 TARGET_KERNEL_SOURCE := kernel/samsung/exynos3470
 
-# Platform
-TARGET_BOARD_PLATFORM := exynos3
-TARGET_SOC := exynos3470
+# Audio
+BOARD_USES_LIBMEDIA_WITH_AUDIOPARAMETER := true
 
 # Bluetooth
-BOARD_HAVE_BLUETOOTH := true
-BOARD_HAVE_BLUETOOTH_BCM := true
-BOARD_BLUEDROID_VENDOR_CONF := device/samsung/gardalte/bluetooth/libbt_vndcfg.txt
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/gardalte/bluetooth
 
-# Graphics
-USE_OPENGL_RENDERER := true
-BOARD_EGL_CFG := device/samsung/gardalte/egl/egl.cfg
-
-# Pre-L Compatibility
-COMMON_GLOBAL_CFLAGS += \
-    -DADD_LEGACY_ACQUIRE_BUFFER_SYMBOL \
-    -DENABLE_NON_PIE_SUPPORT
-
-# Radio
-BOARD_RIL_CLASS := ../../../device/samsung/gardalte/ril
-
 # Recovery
-BOARD_HAS_NO_SELECT_BUTTON := true
 TARGET_RECOVERY_DEVICE_DIRS += device/samsung/gardalte
 TARGET_RECOVERY_FSTAB := device/samsung/gardalte/rootdir/etc/fstab.universal3470
-TARGET_USERIMAGES_USE_EXT4 := true
-
-# Webkit
-ENABLE_WEBGL := true
-
-# WFD
-BOARD_USES_WFD_SERVICE := true
-
-# Wifi
-BOARD_WLAN_DEVICE                := bcmdhd
-BOARD_HAVE_SAMSUNG_WIFI          := true
-BOARD_HOSTAPD_DRIVER             := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_${BOARD_WLAN_DEVICE}
-BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_${BOARD_WLAN_DEVICE}
-WPA_SUPPLICANT_VERSION           := VER_0_8_X
-WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/dhd/parameters/firmware_path"
-WIFI_DRIVER_FW_PATH_STA          := "/system/etc/wifi/bcmdhd_sta.bin"
-WIFI_DRIVER_FW_PATH_AP           := "/system/etc/wifi/bcmdhd_apsta.bin"
-WIFI_DRIVER_FW_PATH_P2P          := "/system/etc/wifi/bcmdhd_p2p.bin"
 
 # Vendor Init
 TARGET_UNIFIED_DEVICE := true
--- device.mk
+++ device.mk
@@ -14,8 +14,6 @@
 # limitations under the License.
 #
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
 # Also get non-open-source specific aspects if available
 $(call inherit-product-if-exists, vendor/samsung/gardalte/gardalte-vendor.mk)
 
@@ -40,100 +38,19 @@ PRODUCT_PACKAGES += \
     ueventd.universal3470.rc
 
 # Audio
-PRODUCT_PACKAGES += \
-    audio.a2dp.default \
-    audio.usb.default \
-    audio.r_submix.default
-
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
-    $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/audio/default_gain.conf:system/etc/default_gain.conff \
-    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
-    $(LOCAL_PATH)/audio/tinyucm.conf:system/etc/tinyucm.conf
-
-# Camera
-PRODUCT_PROPERTY_OVERRIDES += \
-    camera2.portability.force_api=1
-
-# Data workaround
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/enable_data:system/bin/enable_data
-
-# HW composer
-PRODUCT_PACKAGES += \
-    libion
-
-# Keylayouts
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl
-
-# Lights
-PRODUCT_PACKAGES += \
-    lights.exynos3
+    $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf
 
 # Media config
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/media/media_codecs.xml:system/etc/media_codecs.xml \
-    $(LOCAL_PATH)/media/media_profiles.xml:system/etc/media_profiles.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
-
-# Misc
-PRODUCT_PACKAGES += \
-    com.android.future.usb.accessory
+    $(LOCAL_PATH)/media/media_profiles.xml:system/etc/media_profiles.xml
 
 # Radio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/sbin/cbd:root/sbin/cbd
 
-# Samsung
-PRODUCT_PACKAGES += \
-    SamsungServiceMode
-
-# Wifi
-PRODUCT_PACKAGES += \
-    wpa_supplicant \
-    wpa_supplicant.conf
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
-    $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf
-
-# Set default USB interface
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    persist.sys.usb.config=mtp
-
-# Debugging
-ADDITIONAL_DEFAULT_PROPERTIES += \
-    ro.adb.secure=0 \
-    ro.secure=0 \
-    ro.debuggable=1
-
-# System properties
-PRODUCT_PROPERTY_OVERRIDES += \
-    debug.hwui.render_dirty_regions=false \
-    ro.opengles.version=131072 \
-    ro.zygote.disable_gl_preload=true
-
-# Permissions
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
-    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
-    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
-    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
-    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
-    frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
-    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
-    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
-    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
-    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
-    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
-    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
-    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
-    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
-
 # Dalvik VM specific for devices with 1024 MB of RAM
 $(call inherit-product-if-exists, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
+
+# Inherit from smdk3470-common
+$(call inherit-product, device/samsung/smdk3470-common/smdk3470-common.mk)
