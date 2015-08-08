c62453c9ec18c74978f19239f5c3b54910740b61
C457 <android.c357@gmail.com>
2015-08-08 06:13:25 -0500

    gardalte: changes for ubuntu touch

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -37,45 +37,10 @@ TARGET_KERNEL_SOURCE := kernel/samsung/gardalte
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/gardalte/bluetooth
 
-# Camera
-TARGET_PROVIDES_CAMERA_HAL := true
-COMMON_GLOBAL_CFLAGS += -DDISABLE_HW_ID_MATCH_CHECK
-
 # Recovery
-TARGET_RECOVERY_DEVICE_DIRS += device/samsung/gardalte
 TARGET_RECOVERY_FSTAB := device/samsung/gardalte/rootdir/etc/fstab.universal3470
-
-# SELinux
-BOARD_SEPOLICY_DIRS += \
-   device/samsung/gardalte/sepolicy
-
-BOARD_SEPOLICY_UNION += \
-    file_contexts \
-    service_contexts \
-    device.te \
-    domain.te \
-    drmserver.te \
-    file.te \
-    gpsd.te \
-    init.te \
-    mediaserver.te \
-    servicemanager.te \
-    system_app.te \
-    system_server.te \
-    vold.te \
-    wpa.te
 
 # Vendor Init
 TARGET_UNIFIED_DEVICE := true
 TARGET_INIT_VENDOR_LIB := libinit_gardalte
 TARGET_LIBINIT_DEFINES_FILE := device/samsung/gardalte/init/init_gardalte.c
-
-# TWRP
-DEVICE_RESOLUTION := 480x800
-RECOVERY_SDCARD_ON_DATA := true
-BOARD_HAS_NO_REAL_SDCARD := true
-RECOVERY_GRAPHICS_USE_LINELENGTH := true
-TW_BRIGHTNESS_PATH := "/sys/devices/platform/s5p-mipi-dsim.0/backlight/panel/brightness"
-TW_MAX_BRIGHTNESS := 255
-TW_NO_SCREEN_TIMEOUT := true
-TW_HAS_DOWNLOAD_MODE := true
--- cm.mk
+++
@@ -1,6 +0,0 @@
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-$(call inherit-product, device/samsung/gardalte/full_gardalte.mk)
-
-PRODUCT_NAME := cm_gardalte
